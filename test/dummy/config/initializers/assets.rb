cd353da35cea419a10bfdf7dd112091f41d62933
Finn-Lennart Heemeyer <finn@heemeyer.net>
2015-02-17 11:57:26 +0100

    removes deprecation warnings

--- test/dummy/config/initializers/assets.rb
+++ test/dummy/config/initializers/assets.rb
@@ -1,3 +1,4 @@
 Rails.application.config.assets.precompile += %w( teaspoon.css )
 Rails.application.config.assets.precompile += %w( jasmine/1.3.1.js )
 Rails.application.config.assets.precompile += %w( teaspoon-jasmine.js )
+Rails.application.config.assets.precompile += %w( teaspoon-teaspoon.js )
